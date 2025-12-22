fd224adb78a9bd6a1830cfb0bf8db9d851f906a1
Luke <joao.lucas.lucchetta@gmail.com>
2025-12-22 16:24:19 -0300

    fix: test build

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'json',
+  reporter: 'html',
   use: {
     trace: 'on-first-retry',
     ctPort: 3100,
